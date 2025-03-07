323c3b841c2a4f7114e950df6ffeab652e777e16
Thiz <Thizthizzydizzy@gmail.com>
2025-03-07 09:03:29 -0500

    Added jpackage scripts for generating EXE installers

---
+++ package-gui.bat
@@ -0,0 +1,1 @@
+jpackage -n "VR Manager (GUI)" -i "./dist" --main-jar "./VR_Manager.jar" --app-version "1.1.0" --about-url https://github.com/ThizThizzyDizzy/VR-Manager/ --win-per-user-install --win-shortcut
